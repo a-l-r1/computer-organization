28e4b55745cce7a959f78412361337c070f20d48
a-l-r <alralr@buaa.edu.cn>
2019-07-16 20:33:47 +0800

    Added PC and extended philosophy

---
+++ src/datapath/pc.v
@@ -0,0 +1,23 @@
+`include "pc.h"
+
+module pc(
+	input clk, 
+	input [31:0] next_pc, 
+	input enable, 
+	output [31:0] curr_pc
+)
+
+reg [31:0] saved_pc;
+
+initial begin
+	saved_pc = 32'b0;
+end
+
+always @(posedge clk) begin
+	if (enable == PC_ENABLED) begin
+		saved_pc <= curr_pc;
+	end
+end
+
+endmodule
+
---
+++ src/include/pc.h
@@ -0,0 +1,10 @@
+`ifndef PC_H
+`define PC_H
+
+`define PC_ENABLED 1'b1
+`define PC_ENABLE PC_ENABLED
+`define PC_DISABLED 1'b0
+`define PC_DISABLE PC_DISABLED
+
+`endif
+
