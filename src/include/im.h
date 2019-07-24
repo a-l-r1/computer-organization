86c85ae700abca70e5bb3edade8178a65aff90f3
a-l-r <alralr@buaa.edu.cn>
2019-07-24 15:09:22 +0800

    include: fix macro definition syntax when the a macro is defined by a macro
    Wrong: `define SOME_MACRO SOME_EXPLAINING_MACRO
    Right: `define SOME_MACRO `SOME_EXPLAINING_MACRO

--- src/include/im.h
+++ src/include/im.h
@@ -2,9 +2,9 @@
 `define IM_H
 
 `define IM_ENABLE 1`b1
-`define IM_ENABLED IM_ENABLE
+`define IM_ENABLED `IM_ENABLE
 `define IM_DISABLE 1`b0
-`define IM_DISABLED IM_DISABLE
+`define IM_DISABLED `IM_DISABLE
 
 `define IM_ADDR_WIDTH 8
 `define IM_SIZE 64
--- src/include/pc.h
+++ src/include/pc.h
@@ -2,9 +2,9 @@
 `define PC_H
 
 `define PC_ENABLED 1'b1
-`define PC_ENABLE PC_ENABLED
+`define PC_ENABLE `PC_ENABLED
 `define PC_DISABLED 1'b0
-`define PC_DISABLE PC_DISABLED
+`define PC_DISABLE `PC_DISABLED
 
 `endif
 
