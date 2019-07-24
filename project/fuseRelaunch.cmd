fd506c1a35c6aca3150bebbcb3fda0ca6e71d480
a-l-r <alralr@buaa.edu.cn>
2019-07-24 12:49:12 +0800

    project: more intermediate files
    Thanks for wine, I can at least get ISE working and find actual bugs.
    Now it's a verilog-compiler-on-x86_64-on-wine-on-Ubuntu situation.

--- project/fuseRelaunch.cmd
+++ project/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/home/a-l-r/co/src/project/alu_tb_isim_beh.exe" -prj "Z:/home/a-l-r/co/src/project/alu_tb_beh.prj" "work.alu_tb" "work.glbl" 
+-intstyle "ise" -incremental -i "Z:/home/a-l-r/co/src/src/include" -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/home/a-l-r/co/src/project/alu_tb_isim_beh.exe" -prj "Z:/home/a-l-r/co/src/project/alu_tb_beh.prj" "work.alu_tb" "work.glbl" 
