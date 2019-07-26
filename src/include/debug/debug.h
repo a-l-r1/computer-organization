8ff24f00ea4a28c4e27daa18c65f11114c6cb793
a-l-r <alralr@buaa.edu.cn>
2019-07-26 20:30:48 +0800

    debug: implement baby debug printing

--- src/include/debug/debug.h
+++ src/include/debug/debug.h
@@ -12,5 +12,27 @@
 `define normal_display display
 `endif /* DEBUG */
 
+/* Hack */
+
+function void _debug_print(string s);
+	$display("%s", s);
+endfunction
+
+`define DEBUG_PRINT_TRAP
+
+/* TODO: nest it deeper when this hack works */
+
+`define DEBUG_PRINT(p0, p1=EBUG_PRINT_TRAP, p2=EBUG_PRINT_TRAP) \
+	`ifdef D``p1 \ 
+		_debug_print($sprintf(p0)); \
+	`else \
+		`ifdef D``p2 \ 
+			_debug_print($sprintf(p0, p1)); \
+		`else \
+			_debug_print($sprintf(p0, p1, p2)); \
+		`endif \
+	`endif
+
+
 `endif /* DEBUG_H */
 
