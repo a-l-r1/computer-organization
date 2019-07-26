d66842e7dd746b9589378f007e536e4bb61dd659
a-l-r <alralr@buaa.edu.cn>
2019-07-26 21:04:02 +0800

    tests/debug: modify debug-print-test accordingly (2)

--- tests/debug/debug-print-test.v
+++ tests/debug/debug-print-test.v
@@ -7,19 +7,19 @@ initial begin
 	$write("debug printing test begin\n");
 
 	/* one argument */
-	`debug_write("42");
+	`debug_write(("42"));
 	$write("\n");
 
 	/* two arguments */
-	`debug_write("%d", 2 + 2);
+	`debug_write(("%d", 2 + 2));
 	$write("\n");
 
 	/* two arguments w/ a string formatted */
-	`debug_write("%s", "aaa");
+	`debug_write(("%s", "aaa"));
 	$write("\n");
 
 	/* three arguments */
-	`debug_write("%d + %d = 5", 2, 2);
+	`debug_write(("%d + %d = 5", 2, 2));
 	$write("\n");
 end
 
