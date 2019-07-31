24a081996cfadd05a667309c96905f1287be5e3d
a-l-r <alralr@buaa.edu.cn>
2019-07-31 16:09:12 +0800

    src/include/instr-id: implement instruction field detection

--- src/include/instr-id.h
+++ src/include/instr-id.h
@@ -7,6 +7,23 @@
 
 `define ROBUST_FUNCTION function automatic
 
+`define GET_OP(x) (x[31:26])
+`define GET_RS(x) (x[25:21])
+`define GET_RT(x) (x[20:16])
+`define GET_RD(x) (x[15:11])
+`define GET_SHAMT(x) (x[10:6])
+`define GET_FUNCT(x) (x[5:0])
+`define GET_IMM(x) (x[15:0])
+`define GET_IMM_J(x) (x[25:0])
+
+`define IF_OR_UNKNOWN(cond, result) do begin \ 
+	if (cond) begin \ 
+		get_kind = result; \ 
+	end else begin \ 
+		get_kind = `UNKNOWN; \ 
+	end \ 
+	end while(0)
+
 `define UNKNOWN 9'b0000_00000
 `define UNK `UNKNOWN
 `define ADDU 9'b0001_00000
