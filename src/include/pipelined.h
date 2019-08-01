dc61ae907fd37c130ba8261c1ca6c59d0f19d5d2
a-l-r <alralr@buaa.edu.cn>
2019-08-01 15:45:04 +0800

    include/pipelined: initial impl

---
+++ src/include/pipelined.h
@@ -0,0 +1,14 @@
+`ifndef PIPELINED_H
+`define PIPELINED_H
+
+`define FW_D_NONE 2'b00
+`define FW_D_E 2'b01
+`define FW_D_M 2'b10
+`define FW_D_W 2'b11
+
+`define FW_E_NONE 2'b00
+`define FW_E_M 2'b01
+`define FW_E_W 2'b10
+
+`endif
+
