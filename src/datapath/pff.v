4f3eabf5674c54a3c5ac3a2296f4fe4bd94ec874
a-l-r <alralr@buaa.edu.cn>
2019-08-02 17:40:46 +0800

    pff: initial implementation

---
+++ src/datapath/pff.v
@@ -0,0 +1,32 @@
+`include "pff.h"
+
+`define PART_NAME "pff"
+
+`include "debug.h"
+
+module pff(
+	input clk, 
+	input enable, 
+	input [BIT_WIDTH - 1:0] i, 
+	output [BIT_WIDTH - 1:0] o
+)
+
+parameter BIT_WIDTH = 32;
+
+reg [BIT_WIDTH - 1:0] data;
+
+initial begin
+	data <= 0;
+end
+
+always @(posedge clk) begin
+	if (enable == `PFF_ENABLED) begin
+		data <= i;
+	end
+end
+
+assign o = (enable == `PFF_ENABLED) ? data : 
+	0;
+
+endmodule
+
---
+++ src/include/pff.h
@@ -0,0 +1,8 @@
+`ifndef PFF_H
+`define PFF_H
+
+`define PFF_ENABLED 1'b1
+`define PFF_DISABLED 1'b0
+
+`endif
+
