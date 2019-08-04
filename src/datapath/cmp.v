258dde48eb41a13477d38d1254230a3c482faa3d
a-l-r <alralr@buaa.edu.cn>
2019-08-04 20:37:20 +0800

    cmp: initial impl

---
+++ src/datapath/cmp.v
@@ -0,0 +1,32 @@
+`include "alu.h"
+`include "cmp.h"
+
+`define PART_NAME "cmp"
+
+`include "debug/debug.h"
+
+module cmp(
+	input [31:0] reg1, 
+	input [31:0] reg2, 
+	output [1:0] cmp, 
+	output [1:0] sig_cmp, 
+	output [1:0] reg2_sig_cmp
+);
+
+assign cmp = 
+	($unsigned(reg1) > $unsigned(reg2)) ? `CMP_LARGER : 
+	($unsigned(reg1) < $unsigned(reg2)) ? `CMP_SMALLER : 
+	`CMP_EQUAL;
+
+assign sig_cmp = 
+	($signed(reg1) > $signed(reg2)) ? `CMP_LARGER : 
+	($signed(reg1) < $signed(reg2)) ? `CMP_SMALLER : 
+	`CMP_EQUAL;
+
+assign reg2_sig_cmp = 
+	($signed(reg2) > $signed(0)) ? `CMP_LARGER : 
+	($signed(reg2) < $signed(0)) ? `CMP_SMALLER : 
+	`CMP_EQUAL;
+
+endmodule
+
---
+++ src/include/cmp.h
@@ -0,0 +1,11 @@
+`ifndef CMP_H
+`define CMP_H
+
+`include "alu.h"
+
+`define CMP_LARGER `ALU_LARGER
+`define CMP_SMALLER `ALU_SMALLER
+`define CMP_EQUAL `ALU_EQUAL
+
+`endif
+
