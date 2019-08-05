6df6348bc6c462f13ba856024258b86f190a9907
a-l-r <alralr@buaa.edu.cn>
2019-08-06 07:24:07 +0800

    pipelined2: initial impl

---
+++ src/include/pipelined2.h
@@ -0,0 +1,18 @@
+`ifndef PIPELINED_H
+`define PIPELINED_H
+
+`define orig 0
+`define E2D_rf 1
+`define E2D_npc 2
+`define M2D_npc 3
+`define M2D_alu 4
+`define W2D_rf 5
+
+`define M2E_npc 1
+`define M2E_alu 2
+`define W2E_rf 3
+
+`define W2M_rf 1
+
+`endif
+
