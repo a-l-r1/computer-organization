50f99a927249bf3d72056f4c313894aadd223253
a-l-r <alralr@buaa.edu.cn>
2019-08-06 07:03:01 +0800

    tools/simulate-mars: add cygwin support

--- tools/simulate-mars.py
+++ tools/simulate-mars.py
@@ -8,7 +8,7 @@ _mars_path = os.path.dirname(os.path.realpath(__file__)) + '/' + 'mars.jar'
 
 
 def main() -> None:
-    if sys.platform not in ['linux', 'win32']:
+    if sys.platform not in ['linux', 'win32', 'cygwin']:
         raise RuntimeError('OS unsupported')
 
     if len(sys.argv) != 2:
@@ -18,11 +18,16 @@ def main() -> None:
     if not sys.argv[1].endswith('.asm'):
         raise RuntimeError('argv[1] doesn\'t end with .asm')
 
+    # TODO: hack
+    if sys.platform == 'cygwin':
+        _mars_path = 'tools/mars.jar'
+
     # TODO: no more hardcoded mars path
     os.system(' '.join(
         ['java', '-jar', _mars_path, 
             'mc', 'CompactDataAtZero', 
             'nc', 
+            'db', 
             sys.argv[1]
             ]))
 
