c99f331e662c0a262efdc3225ae6aff80a0371ce
a-l-r <alralr@buaa.edu.cn>
2019-08-14 17:17:10 +0800

    pc: implement unaligned related exceptions

--- src/datapath/pc.v
+++ src/datapath/pc.v
@@ -1,5 +1,7 @@
 `include "pc.h"
 
+`include "im.h"
+
 `define PART_NAME "pc"
 
 `include "debug/debug.h"
@@ -8,7 +10,8 @@ module pc(
 	input clk, 
 	input [31:0] next_pc, 
 	input enable, 
-	output [31:0] curr_pc
+	output [31:0] curr_pc, 
+	output invalid
 );
 
 reg [31:0] saved_pc;
@@ -27,7 +30,12 @@ always @(posedge clk) begin
 	end
 end
 
-assign curr_pc = saved_pc;
+assign curr_pc = {saved_pc[31:2], 2'b0};
+
+assign invalid = 
+	(curr_pc[1:0] != 2'b0) || 
+	($unsigned(curr_pc) < $unsigned(`IM_ADDR_LB)) || 
+	($unsigned(curr_pc) > $unsigned(`IM_ADDR_UB));
 
 endmodule
 
