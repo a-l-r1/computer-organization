1b678f602a5b1dcbeb2942992d137d269f71d84c
a-l-r <alralr@buaa.edu.cn>
2019-08-14 17:15:18 +0800

    pipelined3: full control impl

--- src/control/pipelined3.v
+++ src/control/pipelined3.v
@@ -1,12 +1,14 @@
 `include "instr-id.h"
-`include "pipelined2.h"
+`include "pipelined3.h"
 
 `define PART_NAME "instr-id"
 
 `include "debug/debug.h"
 
 `include "alu.h"
+`include "ac.h"
 `include "ext.h"
+`include "cp0.h"
 `include "npc.h"
 `include "md.h"
 `include "dm.h"
@@ -15,49 +17,17 @@
 * in modules, and iSim would refuse to simulate seeing these functions are out
 * of modules. Salute to ISE! */
 
-/*
-`ROBUST_FUNCTION [8:0] kind;
-	input [31:0] instr;
-	reg r;
-	
-	begin
-		r = (`OP(instr) == 6'b000000);
-
-		if (r && `FUNCT(instr) == 6'b100001 && `SHAMT(instr) == 5'b00000) kind = `ADDU;
-		if (r && `FUNCT(instr) == 6'b100011 && `SHAMT(instr) == 5'b00000) kind = `SUBU;
-		if (`OP(instr) == 6'b001111 && `RS(instr) == 5'b00000) kind = `LUI;
-		if (`OP(instr) == 6'b001101) kind = `ORI;
-		if (`OP(instr) == 6'b100011) kind = `LW;
-		if (`OP(instr) == 6'b101011) kind = `SW;
-		if (`OP(instr) == 6'b000100) kind = `BEQ;
-		if (instr == 32'b0) kind = `NOP;
-
-		kind = `UNKNOWN;
-	end
-endfunction
-
-`ROBUST_FUNCTION [3:0] dptype;
-	input [31:0] instr;
-	reg [8:0] kind1;
-	
-	begin
-		kind1 = kind(instr);
-
-		dptype = kind1[8:5];
-	end
-endfunction
-*/
-
 module kind(
 	input [31:0] instr, 
 	output [8:0] result
 );
 
-wire r, regimm;
+wire r, regimm, cop0;
 wire rs_zero, rt_zero, rd_zero, shamt_zero;
 
 assign r = (`OP(instr) == 6'b000000);
 assign regimm = (`OP(instr) == 6'b000001);
+assign cop0 = (`OP(instr) == 6'b010000);
 
 assign rs_zero = (`RS(instr) == 5'b00000);
 assign rt_zero = (`RT(instr) == 5'b00000);
@@ -127,36 +97,71 @@ assign result =
 	(r && `FUNCT(instr) == 6'b010001 && rt_zero && rd_zero && shamt_zero) ? `MTHI : 
 	(r && `FUNCT(instr) == 6'b010011 && rt_zero && rd_zero && shamt_zero) ? `MTLO : 
 
-	`UNKNOWN;
+	(cop0 && `RS(instr) == 5'b00000 && instr[10:3] == 8'b00000000) ? `MFC0 : 
+	(cop0 && `RS(instr) == 5'b00100 && instr[10:3] == 8'b00000000) ? `MTC0 : 
+	(cop0 && instr == 32'h42000018) ? `ERET : 
 
+	`UNKNOWN;
 endmodule
 
 module control(
 	input clk, 
 	input [31:0] d_instr, 
+	input [31:0] e_instr, 
+	input [31:0] m_instr, 
+	input [31:0] w_instr, 
+
 	input [31:0] rf_read_result2, 
 	input e_md_busy, 
+	input [31:0] m_dm_addr, 
+
+	input f_pc_invalid, 
+	input e_alu_sig_overflow, 
+	input [2:0] m_ac_validity, 
+	input [31:0] d_pc_curr_pc, 
+	input [31:0] e_pc_curr_pc, 
+	input [31:0] m_pc_curr_pc, 
+	input have2handle, 
+
 	output cw_f_pc_enable, 
 	output cw_d_pff_enable, 
+	output cw_d_pff_rst, 
 	output cw_e_pff_rst, 
+	output cw_m_pff_rst, 
+	output cw_w_pff_rst, 
+
 	output [3:0] cw_f_npc_jump_mode, 
+
 	output [2:0] cw_d_ext_mode, 
 	output [4:0] cw_d_rf_read_addr1, 
 	output [4:0] cw_d_rf_read_addr2, 
+
 	output cw_e_m_alusrc, 
 	output [4:0] cw_e_alu_op, 
 	output [3:0] cw_e_md_op, 
-	output cw_m_hilo, 
+	output cw_e_m_hilo, 
+	output cw_e_md_stop, 
+	output cw_e_md_restore, 
+
+	output cw_m_m_bridge, 
 	output cw_m_dm_write_enable, 
 	output [2:0] cw_m_dm_mode, 
+	output cw_m_cp0_write_enable, 
+	output cw_m_cp0_exit_isr, 
+	output cw_m_cp0_in_bds, 
+	output [4:0] cw_m_cp0_exc, 
+	output [31:0] cw_m_cp0_curr_pc, 
+
 	output cw_w_rf_write_enable, 
 	output [2:0] cw_w_m_regdata, 
 	output [4:0] cw_w_rf_write_addr, 
-	output [2:0] cw_fm_d1, 
-	output [2:0] cw_fm_d2, 
-	output [2:0] cw_fm_e1, 
-	output [2:0] cw_fm_e2, 
-	output [2:0] cw_fm_m
+
+	output [3:0] cw_fm_d1, 
+	output [3:0] cw_fm_d2, 
+	output [3:0] cw_fm_e1, 
+	output [3:0] cw_fm_e2, 
+	output [2:0] cw_fm_m, 
+	output [2:0] cw_fm_epc
 );
 
 /* Macro definitions */
@@ -174,11 +179,9 @@ module control(
 
 `define inf 3'd7
 
-/* Wire and register declarations */
+`define cp0_epc 14
 
-reg [31:0] e_instr;
-reg [31:0] m_instr;
-reg [31:0] w_instr;
+/* Wire and register declarations */
 
 wire [8:0] dkind;
 wire [8:0] ekind;
@@ -213,13 +216,23 @@ wire [3:0] t_use_reg2;
 wire [3:0] t_new_e;
 wire [3:0] t_new_m;
 
+/* F */
+
+wire [3:0] cw_f_npc_jump_mode_orig;
+
+/* M */
+
+wire cw_m_dm_write_enable_orig;
+
+/* Exception ID pipeline */
+
+reg [4:0] d_exc, e_exc, m_exc;
+
+wire [4:0] m_exc_final;
+
 /* Initial block */
 
 initial begin
-	e_instr = 32'b0;
-	m_instr = 32'b0;
-	w_instr = 32'b0;
-
 	e_reg1 = 5'b0;
 	e_reg2 = 5'b0;
 	e_regw = 5'b0;
@@ -231,6 +244,10 @@ initial begin
 	w_reg1 = 5'b0;
 	w_reg2 = 5'b0;
 	w_regw = 5'b0;
+	
+	d_exc = 5'b0;
+	e_exc = 5'b0;
+	m_exc = 5'b0;
 end
 
 /* Module references */
@@ -263,39 +280,17 @@ assign wdptype = wkind[8:5];
 /* Internal pipeline */
 
 always @(posedge clk) begin
-	if (stall == 1'b1) begin
-		e_instr <= 32'b0;
-		m_instr <= e_instr;
-		w_instr <= m_instr;
+	e_reg1 <= (cw_e_pff_rst == 1'b1) ? 5'b0 : d_reg1;
+	m_reg1 <= (cw_m_pff_rst == 1'b1) ? 5'b0 : e_reg1;
+	w_reg1 <= (cw_w_pff_rst == 1'b1) ? 5'b0 : m_reg1;
 
-		e_reg1 <= 5'b0;
-		m_reg1 <= e_reg1;
-		w_reg1 <= m_reg1;
+	e_reg2 <= (cw_e_pff_rst == 1'b1) ? 5'b0 : d_reg2;
+	m_reg2 <= (cw_m_pff_rst == 1'b1) ? 5'b0 : e_reg2;
+	w_reg2 <= (cw_w_pff_rst == 1'b1) ? 5'b0 : m_reg2;
 
-		e_reg2 <= 5'b0;
-		m_reg2 <= e_reg2;
-		w_reg2 <= m_reg2;
-
-		e_regw <= 5'b0;
-		m_regw <= e_regw;
-		w_regw <= m_regw;
-	end else begin
-		e_instr <= d_instr;
-		m_instr <= e_instr;
-		w_instr <= m_instr;
-
-		e_reg1 <= d_reg1;
-		m_reg1 <= e_reg1;
-		w_reg1 <= m_reg1;
-
-		e_reg2 <= d_reg2;
-		m_reg2 <= e_reg2;
-		w_reg2 <= m_reg2;
-
-		e_regw <= d_regw;
-		m_regw <= e_regw;
-		w_regw <= m_regw;
-	end
+	e_regw <= (cw_e_pff_rst == 1'b1) ? 5'b0 : d_regw;
+	m_regw <= (cw_m_pff_rst == 1'b1) ? 5'b0 : e_regw;
+	w_regw <= (cw_w_pff_rst == 1'b1) ? 5'b0 : m_regw;
 
 	`debug_write(("instructions: D: 0x%08x, E: 0x%08x, M: 0x%08x, W: 0x%08x\n", d_instr, e_instr, m_instr, w_instr));
 	`debug_write(("kind: D: 0b%09b, E: 0b%09b, M: 0b%09b, W: 0b%09b\n", dkind, ekind, mkind, wkind));
@@ -308,7 +303,7 @@ end
 
 /* npc.jump_mode is controlled by the instruction in level E */
 
-assign cw_f_npc_jump_mode = 
+assign cw_f_npc_jump_mode_orig = 
 	(ddptype == `BRANCH) ? (
 		(dkind == `BEQ) ? `NPC_EQUAL : 
 		(dkind == `BNE) ? `NPC_NOT_EQUAL : 
@@ -320,6 +315,7 @@ assign cw_f_npc_jump_mode =
 	) : 
 	(ddptype == `JUMP_I) ? `NPC_J : 
 	(ddptype == `JUMP_R) ? `NPC_REG : 
+	(ddptype == `JUMP_C0) ? `NPC_EPC : 
 	`NPC_JUMP_DISABLED;
 
 assign cw_d_ext_mode = 
@@ -406,7 +402,7 @@ assign cw_e_md_op =
 	) : 
 	`MD_NONE;
 
-assign cw_m_hilo = 
+assign cw_e_m_hilo = 
 	(edptype == `LOAD_M) ? (
 		(ekind == `MFHI) ? 1'b0 : 
 		(ekind == `MFLO) ? 1'b1 : 
@@ -414,7 +410,11 @@ assign cw_m_hilo =
 	) : 
 	1'b0;
 
-assign cw_m_dm_write_enable = 
+assign cw_m_m_bridge = 
+	($unsigned(m_dm_addr) >= `DM_ADDR_LB && $unsigned(m_dm_addr) <= `DM_ADDR_UB) ? 1'b0 : 
+	1'b1;
+
+assign cw_m_dm_write_enable_orig = 
 	(mdptype == `STORE) ? 1'b1 : 
 	1'b0;
 
@@ -436,7 +436,7 @@ assign cw_m_dm_mode =
 	`DM_NONE;
 
 assign cw_w_rf_write_enable = 
-	(wdptype == `CAL_R || wdptype == `CAL_I || wdptype == `LOAD || wdptype == `JUMP_I || wdptype == `JUMP_R || wdptype == `CMOV || wdptype == `LOAD_M) ? 1'b1 : 
+	(wdptype == `CAL_R || wdptype == `CAL_I || wdptype == `LOAD || wdptype == `JUMP_I || wdptype == `JUMP_R || wdptype == `CMOV || wdptype == `LOAD_M || wdptype == `LOAD_C0) ? 1'b1 : 
 	1'b0;
 
 /* See doc/datapath/pipelined.md */
@@ -446,6 +446,7 @@ assign cw_w_m_regdata =
 	(wdptype == `LOAD) ? 3'd2 : 
 	(wdptype == `JUMP_I || wdptype == `JUMP_R) ? 3'd3 : 
 	(wdptype == `LOAD_M) ? 3'd4 : 
+	(wdptype == `LOAD_C0) ? 3'd5 : 
 	3'd0;
 
 assign cw_w_rf_write_addr = w_regw;
@@ -457,7 +458,7 @@ assign d_reg1 =
 	`ZERO;
 
 assign d_reg2 = 
-	(ddptype == `CAL_R || ddptype == `CAL_I || ddptype == `STORE || ddptype == `CMOV || ddptype == `CAL_M) ? `drt : 
+	(ddptype == `CAL_R || ddptype == `STORE || ddptype == `CMOV || ddptype == `CAL_M || ddptype == `STORE_C0) ? `drt : 
 	(ddptype == `BRANCH) ? (
 		(dkind == `BEQ) ? `drt : 
 		(dkind == `BNE) ? `drt : 
@@ -471,7 +472,7 @@ assign d_reg2 =
 
 assign d_regw = 
 	(ddptype == `CAL_R || ddptype == `LOAD_M) ? `drd : 
-	(ddptype == `CAL_I || ddptype == `LOAD) ? `drt : 
+	(ddptype == `CAL_I || ddptype == `LOAD || ddptype == `LOAD_C0) ? `drt : 
 	(ddptype == `JUMP_I) ? (
 		(dkind == `JAL) ? `RA : 
 		(dkind == `J) ? `ZERO : 
@@ -497,6 +498,7 @@ assign cw_fm_d1 =
 	(`fwable(d_reg1, m_regw) && (mdptype == `JUMP_I || mdptype == `JUMP_R)) ? `M2D_npc : 
 	(`fwable(d_reg1, m_regw) && (mdptype == `CAL_R || mdptype == `CAL_I || mdptype == `CMOV)) ? `M2D_alu : 
 	(`fwable(d_reg1, m_regw) && mdptype == `LOAD_M) ? `M2D_md : 
+	(`fwable(d_reg1, m_regw) && mdptype == `LOAD_C0) ? `M2D_cp0 : 
 	(`fwable(d_reg1, w_regw)) ? `W2D_rf : 
 	`orig;
 
@@ -504,6 +506,7 @@ assign cw_fm_e1 =
 	(`fwable(e_reg1, m_regw) && (mdptype == `JUMP_I || mdptype == `JUMP_R)) ? `M2E_npc : 
 	(`fwable(e_reg1, m_regw) && (mdptype == `CAL_R || mdptype == `CAL_I || mdptype == `CMOV)) ? `M2E_alu : 
 	(`fwable(e_reg1, m_regw) && mdptype == `LOAD_M) ? `M2E_md : 
+	(`fwable(e_reg1, m_regw) && mdptype == `LOAD_C0) ? `M2E_cp0 : 
 	(`fwable(e_reg1, w_regw)) ? `W2E_rf : 
 	`orig;
 
@@ -516,6 +519,7 @@ assign cw_fm_d2 =
 	(`fwable(d_reg2, m_regw) && (mdptype == `JUMP_I || mdptype == `JUMP_R)) ? `M2D_npc : 
 	(`fwable(d_reg2, m_regw) && (mdptype == `CAL_R || mdptype == `CAL_I || mdptype == `CMOV)) ? `M2D_alu : 
 	(`fwable(d_reg2, m_regw) && mdptype == `LOAD_M) ? `M2D_md : 
+	(`fwable(d_reg2, m_regw) && mdptype == `LOAD_C0) ? `M2D_cp0 : 
 	(`fwable(d_reg2, w_regw)) ? `W2D_rf : 
 	`orig;
 
@@ -523,11 +527,18 @@ assign cw_fm_e2 =
 	(`fwable(e_reg2, m_regw) && (mdptype == `JUMP_I || mdptype == `JUMP_R)) ? `M2E_npc : 
 	(`fwable(e_reg2, m_regw) && (mdptype == `CAL_R || mdptype == `CAL_I || mdptype == `CMOV)) ? `M2E_alu : 
 	(`fwable(e_reg2, m_regw) && mdptype == `LOAD_M) ? `M2E_md : 
+	(`fwable(e_reg2, m_regw) && mdptype == `LOAD_C0) ? `M2E_cp0 : 
 	(`fwable(e_reg2, w_regw)) ? `W2E_rf : 
 	`orig;
 
 assign cw_fm_m = 
 	(`fwable(m_reg2, w_regw)) ? `W2M_rf : 
+	`orig;
+
+assign cw_fm_epc = 
+	(ddptype == `STORE_C0 && `RD(d_instr) == `cp0_epc && `RD(d_instr) != 0) ? `EPC_D2M_rf : 
+	(edptype == `STORE_C0 && `RD(e_instr) == `cp0_epc && `RD(e_instr) != 0) ? `EPC_E2M_rf : 
+	(mdptype == `STORE_C0 && `RD(m_instr) == `cp0_epc && `RD(m_instr) != 0) ? `EPC_M2M_rf : 
 	`orig;
 
 /* Stall control */
@@ -538,14 +549,14 @@ assign t_use_reg1 =
 	`inf;
 
 assign t_use_reg2 = 
-	(ddptype == `STORE) ? 3'd2 : 
+	(ddptype == `STORE || ddptype == `STORE_C0) ? 3'd2 : 
 	(ddptype == `CAL_R || ddptype == `CAL_I || ddptype == `CAL_M || ddptype == `STORE_M) ? 3'd1 : 
 	(ddptype == `BRANCH || ddptype == `CMOV) ? 3'd0 : 
 	`inf;
 
 assign t_new_e = 
-	(edptype == `LOAD) ? 3'd2 : 
-	(edptype == `CAL_R || edptype == `CAL_I) ? 3'd1 : 
+	(edptype == `LOAD || edptype == `LOAD_C0) ? 3'd2 : 
+	(edptype == `CAL_R || edptype == `CAL_I || edptype == `LOAD_C0) ? 3'd1 : 
 	3'd0;
 
 assign t_new_m = 
@@ -561,15 +572,90 @@ assign stall_md_busy =
 	e_md_busy == 1'b1 && 
 	(ddptype == `CAL_M || ddptype == `LOAD_M || ddptype == `STORE_M);
 
+assign stall_tnew_mtc0_eret = 
+	ddptype == `JUMP_C0 && 
+	(edptype == `STORE_C0 && `RT(e_instr) == `cp0_epc && `RT(e_instr) != 0) && 
+	($unsigned(t_new_m) > $unsigned(0));
+
 assign stall = stall_e2d_reg1 | stall_m2d_reg1 | stall_e2d_reg2 | stall_m2d_reg2 | stall_md_busy;
 
 /* TODO: more thorough stall control? */
 
-assign cw_f_pc_enable = ~stall;
+assign cw_f_pc_enable_orig = ~stall;
 
 assign cw_d_pff_enable = ~stall;
 
-assign cw_e_pff_rst = ~stall;
+assign cw_e_pff_rst_orig = stall;
+
+/* Exception ID pipeline */
+
+always @(posedge clk) begin
+	/* Remember the precedence! */
+	d_exc <= 
+		(cw_d_pff_rst == 1'b1) ? `EXC_NONE : 
+		(stall == 1'b1) ? d_exc : 
+		(ddptype == `JUMP_C0) ? `EXC_NONE : 
+		(f_pc_invalid == 1'b1) ? `EXC_ADEL : 
+		`EXC_NONE;
+	e_exc <= 
+		(cw_e_pff_rst == 1'b1) ? `EXC_NONE : 
+		(stall == 1'b1) ? `EXC_NONE : 
+		(dkind == `UNKNOWN) ? `EXC_RI : 
+		d_exc;
+	m_exc <= 
+		(cw_m_pff_rst == 1'b1) ? `EXC_NONE : 
+		((ekind == `ADD || ekind == `ADDI || ekind == `SUB) && e_alu_sig_overflow == 1'b1) ? `EXC_OV : 
+		e_exc;
+end
+
+assign m_exc_final = 
+	(m_ac_validity != `AC_VALID) ? (
+		(m_ac_validity == `AC_BAD && cw_m_dm_write_enable_orig == 1'b0) ? `EXC_ADEL : 
+		(m_ac_validity == `AC_BAD && cw_m_dm_write_enable_orig == 1'b1) ? `EXC_ADES : 
+		m_exc
+	) : 
+	m_exc;
+
+/* Pipeline resetting */
+
+assign cw_d_pff_rst = (ddptype == `JUMP_C0 && stall == 1'b0) || (have2handle == 1'b1);
+/* cw_e_pff_rst omitted */
+assign cw_m_pff_rst = have2handle;
+assign cw_w_pff_rst = have2handle;
+
+assign cw_e_md_restore = (edptype == `STORE_M || mdptype == `STORE_M) && (have2handle == 1'b1);
+
+assign cw_e_md_stop = (edptype == `CAL_M || mdptype == `CAL_M) && (have2handle == 1'b1);
+
+/* Control signal hooking */
+
+assign cw_m_dm_write_enable = cw_m_dm_write_enable_orig & (~have2handle);
+
+assign cw_m_cp0_write_enable = cw_m_cp0_write_enable_orig & (~have2handle);
+
+assign cw_e_pff_rst = cw_e_pff_rst_orig | have2handle;
+
+assign cw_f_npc_jump_mode = 
+	(have2handle == 1'b1) ? `NPC_ISR : 
+	cw_f_npc_jump_mode_orig;
+
+assign cw_f_pc_enable = cw_f_pc_enable_orig | have2handle;
+
+/* Coprocessor 0 control signals */
+
+assign cw_m_cp0_write_enable_orig = (mdptype == `STORE_C0);
+
+assign cw_m_cp0_exit_isr = (mdptype == `JUMP_C0);
+
+assign cw_m_cp0_in_bds = (wdptype == `JUMP_I || wdptype == `JUMP_R || wdptype == `BRANCH);
+
+assign cw_m_cp0_exc = m_exc_final;
+
+assign cw_m_cp0_curr_pc = 
+	/* Remember the precedence! */
+	(m_pc_curr_pc != 32'b0) ? m_pc_curr_pc : 
+	(e_pc_curr_pc != 32'b0) ? e_pc_curr_pc : 
+	d_pc_curr_pc;
 
 endmodule
 
--- src/include/pipelined3.h
+++ src/include/pipelined3.h
@@ -9,13 +9,19 @@
 `define W2D_rf 5
 `define M2D_md 6
 `define E2D_md 7
+`define M2D_cp0 8
 
 `define M2E_npc 1
 `define M2E_alu 2
 `define W2E_rf 3
 `define M2E_md 4
+`define M2E_cp0 5
 
 `define W2M_rf 1
+
+`define EPC_D2M_rf 1
+`define EPC_E2M_rf 2
+`define EPC_M2M_rf 3
 
 `endif
 
