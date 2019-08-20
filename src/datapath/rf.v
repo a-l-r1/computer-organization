8be2b58d791e28e74b565ca24d0db0f9ebd08e8b
a-l-r <alralr@buaa.edu.cn>
2019-08-20 18:10:39 +0800

    rf: implement reset mechanism

--- src/datapath/rf.v
+++ src/datapath/rf.v
@@ -31,12 +31,45 @@ always @(posedge clk) begin
 	`debug_write(("read_result1 = 0x%08x, read_result2 = 0x%08x, write_data = 0x%08x\n", read_result1, read_result2, write_data));
 end
 
-
 always @(posedge clk) begin
-	if (write_enable == `RF_WRITE_ENABLED) begin
-		if (write_addr != `RF_ADDR_ZERO) begin
-			`normal_display((`RF_OUTPUT_FORMAT, $time, curr_pc, write_addr, write_data));
-			registers[write_addr] <= write_data;
+	if (rst == 1'b1) begin
+		registers[1] <= 0;
+		registers[2] <= 0;
+		registers[3] <= 0;
+		registers[4] <= 0;
+		registers[5] <= 0;
+		registers[6] <= 0;
+		registers[7] <= 0;
+		registers[8] <= 0;
+		registers[9] <= 0;
+		registers[10] <= 0;
+		registers[11] <= 0;
+		registers[12] <= 0;
+		registers[13] <= 0;
+		registers[14] <= 0;
+		registers[15] <= 0;
+		registers[16] <= 0;
+		registers[17] <= 0;
+		registers[18] <= 0;
+		registers[19] <= 0;
+		registers[20] <= 0;
+		registers[21] <= 0;
+		registers[22] <= 0;
+		registers[23] <= 0;
+		registers[24] <= 0;
+		registers[25] <= 0;
+		registers[26] <= 0;
+		registers[27] <= 0;
+		registers[28] <= 0;
+		registers[29] <= 0;
+		registers[30] <= 0;
+		registers[31] <= 0;
+	end else begin
+		if (write_enable == `RF_WRITE_ENABLED) begin
+			if (write_addr != `RF_ADDR_ZERO) begin
+				`normal_display((`RF_OUTPUT_FORMAT, $time, curr_pc, write_addr, write_data));
+				registers[write_addr] <= write_data;
+			end
 		end
 	end
 end
