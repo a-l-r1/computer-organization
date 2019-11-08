d28990615e2701e2a1b58f4ffa65f905ce634c16
a-l-r <alralr@buaa.edu.cn>
2019-11-08 23:11:51 +0000

    project: update project files

--- project/isim/single_cycle_tb_isim_beh.exe.sim/work/m_02869494925477708609_3877310806.c
+++ project/isim/single_cycle_tb_isim_beh.exe.sim/work/m_02869494925477708609_3877310806.c
@@ -26,7 +26,61 @@ static int ng1[] = {4, 0};
 
 
 
-static void Cont_59_0(char *t0)
+static void Cont_23_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    unsigned int t8;
+    unsigned int t9;
+    char *t10;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
+    unsigned int t14;
+    unsigned int t15;
+    char *t16;
+
+LAB0:    t1 = (t0 + 6520U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(23, ng0);
+    t2 = (t0 + 1208U);
+    t3 = *((char **)t2);
+    t2 = (t0 + 7448);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t3 + 4);
+    t11 = *((unsigned int *)t3);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t2, 0, 0);
+    t16 = (t0 + 7336);
+    *((int *)t16) = 1;
+
+LAB1:    return;
+}
+
+static void Cont_62_1(char *t0)
 {
     char *t1;
     char *t2;
@@ -37,30 +91,30 @@ static void Cont_59_0(char *t0)
     char *t7;
     char *t8;
 
-LAB0:    t1 = (t0 + 6680U);
+LAB0:    t1 = (t0 + 6768U);
     t2 = *((char **)t1);
     if (t2 == 0)
         goto LAB2;
 
 LAB3:    goto *t2;
 
-LAB2:    xsi_set_current_line(59, ng0);
-    t2 = (t0 + 3768U);
+LAB2:    xsi_set_current_line(62, ng0);
+    t2 = (t0 + 3928U);
     t3 = *((char **)t2);
-    t2 = (t0 + 7344);
+    t2 = (t0 + 7512);
     t4 = (t2 + 56U);
     t5 = *((char **)t4);
     t6 = (t5 + 56U);
     t7 = *((char **)t6);
     memcpy(t7, t3, 8);
     xsi_driver_vfirst_trans(t2, 0, 31);
-    t8 = (t0 + 7248);
+    t8 = (t0 + 7352);
     *((int *)t8) = 1;
 
 LAB1:    return;
 }
 
-static void Cont_110_1(char *t0)
+static void Cont_112_2(char *t0)
 {
     char t6[8];
     char *t1;
@@ -73,36 +127,36 @@ static void Cont_110_1(char *t0)
     char *t11;
     char *t12;
 
-LAB0:    t1 = (t0 + 6928U);
+LAB0:    t1 = (t0 + 7016U);
     t2 = *((char **)t1);
     if (t2 == 0)
         goto LAB2;
 
 LAB3:    goto *t2;
 
-LAB2:    xsi_set_current_line(110, ng0);
-    t2 = (t0 + 3608U);
+LAB2:    xsi_set_current_line(112, ng0);
+    t2 = (t0 + 3768U);
     t4 = *((char **)t2);
     t2 = ((char*)((ng1)));
     memset(t6, 0, 8);
     xsi_vlog_unsigned_add(t6, 32, t4, 32, t2, 32);
-    t7 = (t0 + 7408);
+    t7 = (t0 + 7576);
     t8 = (t7 + 56U);
     t9 = *((char **)t8);
     t10 = (t9 + 56U);
     t11 = *((char **)t10);
     memcpy(t11, t6, 8);
     xsi_driver_vfirst_trans(t7, 0, 31);
-    t12 = (t0 + 7264);
+    t12 = (t0 + 7368);
     *((int *)t12) = 1;
 
 LAB1:    return;
 }
 
 
-extern void work_m_00734490075165958695_3877310806_init()
+extern void work_m_02869494925477708609_3877310806_init()
 {
-	static char *pe[] = {(void *)Cont_59_0,(void *)Cont_110_1};
-	xsi_register_didat("work_m_00734490075165958695_3877310806", "isim/single_cycle_tb_isim_beh.exe.sim/work/m_00734490075165958695_3877310806.didat");
+	static char *pe[] = {(void *)Cont_23_0,(void *)Cont_62_1,(void *)Cont_112_2};
+	xsi_register_didat("work_m_02869494925477708609_3877310806", "isim/single_cycle_tb_isim_beh.exe.sim/work/m_02869494925477708609_3877310806.didat");
 	xsi_register_executes(pe);
 }
