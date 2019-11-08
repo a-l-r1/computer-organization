2d6ce5ca1a0b630ed721eff4b3d0e0365ff292e4
a-l-r <alralr@buaa.edu.cn>
2019-11-08 15:24:56 +0000

    pc: [reset] implement reset mechanism

--- src/datapath/pc.v
+++ src/datapath/pc.v
@@ -6,6 +6,7 @@
 
 module pc(
 	input clk, 
+	input rst, 
 	input [31:0] next_pc, 
 	input enable, 
 	output [31:0] curr_pc
@@ -22,8 +23,12 @@ initial begin
 end
 
 always @(posedge clk) begin
-	if (enable == `PC_ENABLED) begin
-		saved_pc <= next_pc;
+	if (rst == 1'b1) begin
+		saved_pc <= `PC_START_ADDRESS:
+	end else begin
+		if (enable == `PC_ENABLED) begin
+			saved_pc <= next_pc;
+		end
 	end
 end
 
