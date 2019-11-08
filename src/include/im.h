497e20924dbe70573c0972cc46c2d076846bf755
a-l-r <alralr@buaa.edu.cn>
2019-11-08 15:34:41 +0000

    im: [compliance] fix IM_SIZE and IM_ADDR_WIDTH

--- src/include/im.h
+++ src/include/im.h
@@ -6,8 +6,8 @@
 `define IM_DISABLE 1'b0
 `define IM_DISABLED `IM_DISABLE
 
-`define IM_ADDR_WIDTH 8
-`define IM_SIZE 64
+`define IM_ADDR_WIDTH 10
+`define IM_SIZE 1024
 
 `define IM_CODE_FILENAME "code.hex"
 
