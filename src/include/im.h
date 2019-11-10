43c6b6818fa50b51a3cf4ac9637a70c36cb3a7a7
a-l-r <alralr@buaa.edu.cn>
2019-11-10 15:40:15 +0000

    include: [cruft-removal] remove silly crufty aliases

--- src/include/im.h
+++ src/include/im.h
@@ -1,10 +1,8 @@
 `ifndef IM_H
 `define IM_H
 
-`define IM_ENABLE 1'b1
-`define IM_ENABLED `IM_ENABLE
-`define IM_DISABLE 1'b0
-`define IM_DISABLED `IM_DISABLE
+`define IM_ENABLED 1'b1
+`define IM_DISABLED 1'b0
 
 `define IM_ADDR_WIDTH 12
 `define IM_SIZE 1024
