bcc5f583ee1fb571b3410ef54e295281173eed25
a-l-r <alralr@buaa.edu.cn>
2019-11-24 20:32:43 +0000

    project: update project files

--- project/isim/pipelined2_tb_isim_beh.exe.sim/work/m_08273487103722745032_3037777339.c
+++ project/isim/pipelined2_tb_isim_beh.exe.sim/work/m_08273487103722745032_3037777339.c
@@ -40239,40 +40239,22 @@ LAB9:    t15 = *((unsigned int *)t3);
 
 static void Cont_572_39(char *t0)
 {
-    char t3[8];
     char *t1;
     char *t2;
+    char *t3;
     char *t4;
-    unsigned int t5;
-    unsigned int t6;
-    unsigned int t7;
+    char *t5;
+    char *t6;
+    char *t7;
     unsigned int t8;
     unsigned int t9;
     char *t10;
-    char *t11;
-    char *t12;
-    unsigned int t13;
+    unsigned int t11;
+    unsigned int t12;
+    char *t13;
     unsigned int t14;
     unsigned int t15;
-    unsigned int t16;
-    unsigned int t17;
-    unsigned int t18;
-    unsigned int t19;
-    unsigned int t20;
-    char *t21;
-    char *t22;
-    char *t23;
-    char *t24;
-    char *t25;
-    unsigned int t26;
-    unsigned int t27;
-    char *t28;
-    unsigned int t29;
-    unsigned int t30;
-    char *t31;
-    unsigned int t32;
-    unsigned int t33;
-    char *t34;
+    char *t16;
 
 LAB0:    t1 = (t0 + 20992U);
     t2 = *((char **)t1);
@@ -40283,71 +40265,30 @@ LAB3:    goto *t2;
 
 LAB2:    xsi_set_current_line(572, ng0);
     t2 = (t0 + 6808U);
-    t4 = *((char **)t2);
-    memset(t3, 0, 8);
-    t2 = (t4 + 4);
-    t5 = *((unsigned int *)t2);
-    t6 = (~(t5));
-    t7 = *((unsigned int *)t4);
-    t8 = (t7 & t6);
-    t9 = (t8 & 1U);
-    if (t9 != 0)
-        goto LAB7;
-
-LAB5:    if (*((unsigned int *)t2) == 0)
-        goto LAB4;
-
-LAB6:    t10 = (t3 + 4);
-    *((unsigned int *)t3) = 1;
-    *((unsigned int *)t10) = 1;
-
-LAB7:    t11 = (t3 + 4);
-    t12 = (t4 + 4);
-    t13 = *((unsigned int *)t4);
-    t14 = (~(t13));
-    *((unsigned int *)t3) = t14;
-    *((unsigned int *)t11) = 0;
-    if (*((unsigned int *)t12) != 0)
-        goto LAB9;
-
-LAB8:    t19 = *((unsigned int *)t3);
-    *((unsigned int *)t3) = (t19 & 1U);
-    t20 = *((unsigned int *)t11);
-    *((unsigned int *)t11) = (t20 & 1U);
-    t21 = (t0 + 24368);
-    t22 = (t21 + 56U);
-    t23 = *((char **)t22);
-    t24 = (t23 + 56U);
-    t25 = *((char **)t24);
-    memset(t25, 0, 8);
-    t26 = 1U;
-    t27 = t26;
-    t28 = (t3 + 4);
-    t29 = *((unsigned int *)t3);
-    t26 = (t26 & t29);
-    t30 = *((unsigned int *)t28);
-    t27 = (t27 & t30);
-    t31 = (t25 + 4);
-    t32 = *((unsigned int *)t25);
-    *((unsigned int *)t25) = (t32 | t26);
-    t33 = *((unsigned int *)t31);
-    *((unsigned int *)t31) = (t33 | t27);
-    xsi_driver_vfirst_trans(t21, 0, 0);
-    t34 = (t0 + 21920);
-    *((int *)t34) = 1;
+    t3 = *((char **)t2);
+    t2 = (t0 + 24368);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memset(t7, 0, 8);
+    t8 = 1U;
+    t9 = t8;
+    t10 = (t3 + 4);
+    t11 = *((unsigned int *)t3);
+    t8 = (t8 & t11);
+    t12 = *((unsigned int *)t10);
+    t9 = (t9 & t12);
+    t13 = (t7 + 4);
+    t14 = *((unsigned int *)t7);
+    *((unsigned int *)t7) = (t14 | t8);
+    t15 = *((unsigned int *)t13);
+    *((unsigned int *)t13) = (t15 | t9);
+    xsi_driver_vfirst_trans(t2, 0, 0);
+    t16 = (t0 + 21920);
+    *((int *)t16) = 1;
 
 LAB1:    return;
-LAB4:    *((unsigned int *)t3) = 1;
-    goto LAB7;
-
-LAB9:    t15 = *((unsigned int *)t3);
-    t16 = *((unsigned int *)t12);
-    *((unsigned int *)t3) = (t15 | t16);
-    t17 = *((unsigned int *)t11);
-    t18 = *((unsigned int *)t12);
-    *((unsigned int *)t11) = (t17 | t18);
-    goto LAB8;
-
 }
 
 
