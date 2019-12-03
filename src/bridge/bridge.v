76d396e6d0ba3cf305a3b1aff29b0ad6a98f06da
a-l-r <alralr@buaa.edu.cn>
2019-12-03 19:19:50 +0000

    bridge: change address resolution scheme

--- src/bridge/bridge.v
+++ src/bridge/bridge.v
@@ -12,34 +12,18 @@ module bridge(
 );
 
 wire [3:0] curr_dev;
+
+wire timer0_write_enable, timer1_write_enable;
+/* NOTE: timer[01]_addr are formal addresses (32-bit). Address slicing is done
+ * at the declarations of the corresponding input ports of timer[01]. */
+wire [31:0] timer0_addr, timer1_addr;
+wire [31:0] timer0_read_result, timer1_read_result;
+wire timer0_irq, timer1_irq;
+
 assign curr_dev = 
 	($unsigned(addr) >= $unsigned(`BRIDGE_TIMER0_LB) && $unsigned(addr) <= $unsigned(`BRIDGE_TIMER0_UB)) ? 4'd0 : 
 	($unsigned(addr) >= $unsigned(`BRIDGE_TIMER1_LB) && $unsigned(addr) <= $unsigned(`BRIDGE_TIMER1_UB)) ? 4'd1 : 
 	4'd15;
-
-wire [31:0] timer0_read_result, timer1_read_result;
-wire timer0_write_enable, timer1_write_enable;
-wire timer0_irq, timer1_irq;
-
-timer #(.BASE(`BRIDGE_TIMER0_BASE)) timer0 (
-	.clk(clk), 
-	.rst(rst), 
-	.addr(addr), 
-	.write_enable(timer0_write_enable), 
-	.write_data(write_data), 
-	.read_result(timer0_read_result), 
-	.irq(timer0_irq)
-);
-
-timer #(.BASE(`BRIDGE_TIMER1_BASE)) timer1 (
-	.clk(clk), 
-	.rst(rst), 
-	.addr(addr), 
-	.write_enable(timer1_write_enable), 
-	.write_data(write_data), 
-	.read_result(timer1_read_result), 
-	.irq(timer1_irq)
-);
 
 assign read_result = 
 	(curr_dev == 4'd0) ? timer0_read_result : 
@@ -47,9 +31,36 @@ assign read_result =
 	32'b0;
 
 assign timer0_write_enable = (write_enable && (curr_dev == 4'd0));
+assign timer0_addr = 
+	(curr_dev == 4'd0) ? $unsigned(addr) - $unsigned(`BRIDGE_TIMER0_BASE) : 
+	2'b0;
+
 assign timer1_write_enable = (write_enable && (curr_dev == 4'd1));
+assign timer1_addr = 
+	(curr_dev == 4'd1) ? $unsigned(addr) - $unsigned(`BRIDGE_TIMER1_BASE) : 
+	2'b0;
 
 assign hwirq = {4'b0, timer1_irq, timer0_irq};
+
+timer timer0 (
+	.clk(clk), 
+	.rst(rst), 
+	.addr(timer0_addr[3:2]), 
+	.write_enable(timer0_write_enable), 
+	.write_data(write_data), 
+	.read_result(timer0_read_result), 
+	.irq(timer0_irq)
+);
+
+timer timer1 (
+	.clk(clk), 
+	.rst(rst), 
+	.addr(timer1_addr[3:2]), 
+	.write_enable(timer1_write_enable), 
+	.write_data(write_data), 
+	.read_result(timer1_read_result), 
+	.irq(timer1_irq)
+);
 
 endmodule
 
