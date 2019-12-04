6b62896ac314950920d21c945e17eb1b1a89dd9e
a-l-r <alralr@buaa.edu.cn>
2019-12-04 20:38:42 +0000

    mips: adapt to new address validity checking architecture

--- src/mips/mips.v
+++ src/mips/mips.v
@@ -13,18 +13,20 @@ wire [31:0] cpu_write_data, cpu_read_result;
 wire [7:2] hwirq;
 wire [2:0] dm_mode;
 
-wire dev_write_enable;
+wire dev_write_enable, bridge_valid, bridge_stop;
 
 cpu cpu(
 	.clk(clk), 
 	.rst(rst), 
 	.cpu_read_result(cpu_read_result), 
 	.hwirq(hwirq), 
+	.bridge_valid(bridge_valid), 
 	.cpu_addr(cpu_addr), 
 	.dev_write_enable(dev_write_enable), 
 	.dm_mode(dm_mode), 
 	.cpu_write_data(cpu_write_data), 
-	.test_addr(addr)
+	.test_addr(addr), 
+	.bridge_stop(bridge_stop)
 );
 
 bridge bridge(
@@ -36,7 +38,9 @@ bridge bridge(
 	.write_data(cpu_write_data), 
 	.test_interrupt(interrupt), 
 	.read_result(cpu_read_result), 
-	.hwirq(hwirq)
+	.hwirq(hwirq), 
+	.valid(bridge_valid), 
+	.stop(bridge_stop)
 );
 
 endmodule
