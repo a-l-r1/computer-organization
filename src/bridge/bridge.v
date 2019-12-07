226b2fc1e5f0e27cd7e4c8b73ed35eeeb5564888
a-l-r <alralr@buaa.edu.cn>
2019-12-07 15:56:34 +0000

    mips: move timer0 and timer1 to top-level module

--- src/bridge/bridge.v
+++ src/bridge/bridge.v
@@ -4,35 +4,37 @@
 module bridge(
 	input clk, 
 	input rst, 
+	input [2:0] dm_mode, 
+	output valid, 
+	input stop, 
+
 	input [31:0] addr, 
 	input write_enable, 
-	input [2:0] dm_mode, 
 	input [31:0] write_data, 
-	input test_interrupt, 
-	input stop, 
 	output [31:0] read_result, 
 	output [7:2] hwirq, 
-	output valid
+
+	output [31:0] timer0_addr, 
+	output timer0_write_enable, 
+	output [31:0] timer0_write_data, 
+	input [31:0] timer0_read_result, 
+	input timer0_irq, 
+
+	output [31:0] timer1_addr, 
+	output timer1_write_enable, 
+	output [31:0] timer1_write_data, 
+	input [31:0] timer1_read_result, 
+	input timer1_irq, 
+
+	input test_interrupt
 );
 
 wire [3:0] curr_dev;
-
-wire timer0_write_enable, timer1_write_enable;
-/* NOTE: timer[01]_addr are formal addresses (32-bit). Address slicing is done
- * at the declarations of the corresponding input ports of timer[01]. */
-wire [31:0] timer0_addr, timer1_addr;
-wire [31:0] timer0_read_result, timer1_read_result;
-wire timer0_irq, timer1_irq;
 
 assign curr_dev = 
 	($unsigned(addr) >= $unsigned(`BRIDGE_TIMER0_LB) && $unsigned(addr) <= $unsigned(`BRIDGE_TIMER0_UB)) ? 4'd0 : 
 	($unsigned(addr) >= $unsigned(`BRIDGE_TIMER1_LB) && $unsigned(addr) <= $unsigned(`BRIDGE_TIMER1_UB)) ? 4'd1 : 
 	4'd15;
-
-assign read_result = 
-	(curr_dev == 4'd0) ? timer0_read_result : 
-	(curr_dev == 4'd1) ? timer1_read_result : 
-	32'b0;
 
 assign valid = 
 	(dm_mode == `DM_NONE) || 
@@ -50,45 +52,36 @@ assign valid =
 		))
 	);
 
+assign timer0_addr = 
+	(curr_dev == 4'd0) ? $unsigned(addr) - $unsigned(`BRIDGE_TIMER0_BASE) : 
+	2'b0;
+
 assign timer0_write_enable = 
 	(valid == 1'b1) && 
 	(write_enable == 1'b1) && 
 	(stop == 1'b0) && 
 	(curr_dev == 4'd0);
-assign timer0_addr = 
-	(curr_dev == 4'd0) ? $unsigned(addr) - $unsigned(`BRIDGE_TIMER0_BASE) : 
-	2'b0;
+
+assign timer0_write_data = write_data;
 
 assign timer1_write_enable = 
 	(valid == 1'b1) && 
 	(write_enable == 1'b1) && 
 	(stop == 1'b0) && 
 	(curr_dev == 4'd1);
+
 assign timer1_addr = 
 	(curr_dev == 4'd1) ? $unsigned(addr) - $unsigned(`BRIDGE_TIMER1_BASE) : 
 	2'b0;
 
+assign timer1_write_data = write_data;
+
+assign read_result = 
+	(curr_dev == 4'd0) ? timer0_read_result : 
+	(curr_dev == 4'd1) ? timer1_read_result : 
+	32'b0;
+
 assign hwirq = {3'b0, test_interrupt, timer1_irq, timer0_irq};
-
-timer timer0 (
-	.clk(clk), 
-	.rst(rst), 
-	.addr(timer0_addr[3:2]), 
-	.write_enable(timer0_write_enable), 
-	.write_data(write_data), 
-	.read_result(timer0_read_result), 
-	.irq(timer0_irq)
-);
-
-timer timer1 (
-	.clk(clk), 
-	.rst(rst), 
-	.addr(timer1_addr[3:2]), 
-	.write_enable(timer1_write_enable), 
-	.write_data(write_data), 
-	.read_result(timer1_read_result), 
-	.irq(timer1_irq)
-);
 
 endmodule
 
--- src/mips/mips.v
+++ src/mips/mips.v
@@ -8,12 +8,22 @@ module mips(
 wire rst;
 assign rst = reset;
 
+/* cpu <-> bridge */
+
 wire [31:0] cpu_addr;
 wire [31:0] cpu_write_data, cpu_read_result;
 wire [7:2] hwirq;
 wire [2:0] dm_mode;
 
 wire dev_write_enable, bridge_valid, bridge_stop;
+
+/* bridge <-> timers */
+
+wire [31:0] timer0_addr, timer1_addr;
+wire [31:0] timer0_write_data, timer1_write_data;
+wire [31:0] timer0_read_result, timer1_read_result;
+wire timer0_write_enable, timer1_write_enable;
+wire timer0_irq, timer1_irq;
 
 cpu cpu(
 	.clk(clk), 
@@ -29,18 +39,58 @@ cpu cpu(
 	.bridge_stop(bridge_stop)
 );
 
+/* NOTE: timer[01]_addr are formal addresses (32-bit). Address slicing is done
+ * at the declarations of the corresponding input ports of timer[01]. */
+
 bridge bridge(
 	.clk(clk), 
 	.rst(rst), 
+
+	.dm_mode(dm_mode), 
+	.valid(bridge_valid), 
+	.stop(bridge_stop), 
+
 	.addr(cpu_addr), 
 	.write_enable(dev_write_enable), 
-	.dm_mode(dm_mode), 
 	.write_data(cpu_write_data), 
-	.test_interrupt(interrupt), 
 	.read_result(cpu_read_result), 
 	.hwirq(hwirq), 
-	.valid(bridge_valid), 
-	.stop(bridge_stop)
+
+	.timer0_addr(timer0_addr), 
+	.timer0_write_enable(timer0_write_enable), 
+	.timer0_write_data(timer0_write_data), 
+	.timer0_read_result(timer0_read_result), 
+	.timer0_irq(timer0_irq), 
+
+	.timer1_addr(timer1_addr), 
+	.timer1_write_enable(timer1_write_enable), 
+	.timer1_write_data(timer1_write_data), 
+	.timer1_read_result(timer1_read_result), 
+	.timer1_irq(timer1_irq), 
+
+	.test_interrupt(interrupt)
+);
+
+timer timer0 (
+	.clk(clk), 
+	.rst(rst), 
+
+	.addr(timer0_addr[3:2]), 
+	.write_enable(timer0_write_enable), 
+	.write_data(timer0_write_data), 
+	.read_result(timer0_read_result), 
+	.irq(timer0_irq)
+);
+
+timer timer1 (
+	.clk(clk), 
+	.rst(rst), 
+
+	.addr(timer1_addr[3:2]), 
+	.write_enable(timer1_write_enable), 
+	.write_data(timer1_write_data), 
+	.read_result(timer1_read_result), 
+	.irq(timer1_irq)
 );
 
 endmodule
