6051305d5697a49c30d559b04207da718dbcbc90
a-l-r <alralr@buaa.edu.cn>
2019-12-19 19:36:53 +0000

    test: fix memory layout for nixie_demo

--- tests/cpu/pipelined3/nixie_demo.asm
+++ tests/cpu/pipelined3/nixie_demo.asm
@@ -2,13 +2,9 @@
 lui $t0, 0x0000
 ori $t0, 0x7f38
 
-lui $t1, 0x0000
+lui $t1, 0x1737
 ori $t1, 0x7017
 sw $t1, 0($t0)
-
-lui $t1, 0x0000
-ori $t1, 0x1737
-sw $t1, 4($t0)
 
 lui $t1, 0x0000
 ori $t1, 0x000a
