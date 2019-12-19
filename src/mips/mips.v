09dfd85b1d0e8253197c979dbeb44d45e62b3919
a-l-r <alralr@buaa.edu.cn>
2019-12-19 19:22:19 +0000

    mips: add devices

--- src/mips/mips.v
+++ src/mips/mips.v
@@ -1,11 +1,34 @@
 module mips(
 	input clk_in, 
-	input sys_rstn
+	input sys_rstn, 
+	
+	input uart_rxd, 
+	output uart_txd, 
+
+	input [7:0] dip_switch0,
+	input [7:0] dip_switch1,
+	input [7:0] dip_switch2,
+	input [7:0] dip_switch3,
+	input [7:0] dip_switch4,
+	input [7:0] dip_switch5,
+	input [7:0] dip_switch6,
+	input [7:0] dip_switch7,
+
+	output [31:0] led_light, 
+
+	output [7:0] digital_tube0, 
+	output [3:0] digital_tube_sel0, 
+	output [7:0] digital_tube1, 
+	output [3:0] digital_tube_sel1, 
+	output [7:0] digital_tube2, 
+	output digital_tube_sel2, 
+
+	input [7:0] user_key
 );
 
 wire clk, rst;
 assign clk = clk_in;
-assign rst = sys_rstn;
+assign rst = ~sys_rstn;
 
 /* cpu <-> bridge */
 
@@ -16,13 +39,25 @@ wire [2:0] dm_mode;
 
 wire dev_write_enable, bridge_valid, bridge_stop;
 
-/* bridge <-> timers */
+/* bridge <-> devices */
 
-wire [31:0] timer0_addr, timer1_addr;
-wire [31:0] timer0_write_data, timer1_write_data;
-wire [31:0] timer0_read_result, timer1_read_result;
-wire timer0_write_enable, timer1_write_enable;
-wire timer0_irq, timer1_irq;
+wire [31:0] timer_addr, timer_write_data, timer_read_result;
+wire timer_write_enable, timer_irq;
+
+wire [31:0] uart_addr, uart_write_data, uart_read_result;
+wire uart_write_enable, uart_irq;
+
+wire [31:0] switches_addr, switches_write_data, switches_read_result;
+wire switches_write_enable, switches_irq;
+
+wire [31:0] led_addr, led_write_data, led_read_result;
+wire led_write_enable, led_irq;
+
+wire [31:0] nixie_addr, nixie_write_data, nixie_read_result;
+wire nixie_write_enable, nixie_irq;
+
+wire [31:0] buttons_addr, buttons_write_data, buttons_read_result;
+wire buttons_write_enable, buttons_irq;
 
 cpu cpu(
 	.clk(clk), 
@@ -57,41 +92,119 @@ bridge bridge(
 	.read_result(cpu_read_result), 
 	.hwirq(hwirq), 
 
-	.timer0_addr(timer0_addr), 
-	.timer0_write_enable(timer0_write_enable), 
-	.timer0_write_data(timer0_write_data), 
-	.timer0_read_result(timer0_read_result), 
-	.timer0_irq(timer0_irq), 
+	.timer_addr(timer_addr), 
+	.timer_write_enable(timer_write_enable), 
+	.timer_write_data(timer_write_data), 
+	.timer_read_result(timer_read_result), 
+	.timer_irq(timer_irq), 
 
-	.timer1_addr(timer1_addr), 
-	.timer1_write_enable(timer1_write_enable), 
-	.timer1_write_data(timer1_write_data), 
-	.timer1_read_result(timer1_read_result), 
-	.timer1_irq(timer1_irq), 
+	.uart_addr(uart_addr), 
+	.uart_write_enable(uart_write_enable), 
+	.uart_write_data(uart_write_data), 
+	.uart_read_result(uart_read_result), 
+	.uart_irq(uart_irq), 
 
-	.test_interrupt(1'b0)
+	.switches_addr(switches_addr), 
+	.switches_write_enable(switches_write_enable), 
+	.switches_write_data(switches_write_data), 
+	.switches_read_result(switches_read_result), 
+	.switches_irq(switches_irq), 
+
+	.led_addr(led_addr), 
+	.led_write_enable(led_write_enable), 
+	.led_write_data(led_write_data), 
+	.led_read_result(led_read_result), 
+	.led_irq(led_irq), 
+
+	.nixie_addr(nixie_addr), 
+	.nixie_write_enable(nixie_write_enable), 
+	.nixie_write_data(nixie_write_data), 
+	.nixie_read_result(nixie_read_result), 
+	.nixie_irq(nixie_irq), 
+
+	.buttons_addr(buttons_addr), 
+	.buttons_write_enable(buttons_write_enable), 
+	.buttons_write_data(buttons_write_data), 
+	.buttons_read_result(buttons_read_result), 
+	.buttons_irq(buttons_irq)
 );
 
-timer timer0 (
+timer timer(
 	.clk(clk), 
 	.rst(rst), 
 
-	.addr(timer0_addr[3:2]), 
-	.write_enable(timer0_write_enable), 
-	.write_data(timer0_write_data), 
-	.read_result(timer0_read_result), 
-	.irq(timer0_irq)
+	.addr(timer_addr[3:2]), 
+	.write_enable(timer_write_enable), 
+	.write_data(timer_write_data), 
+	.read_result(timer_read_result), 
+	.irq(timer_irq)
 );
 
-timer timer1 (
+/* TODO: uart */
+assign uart_txd = 1'b0;
+
+switches switches(
 	.clk(clk), 
 	.rst(rst), 
 
-	.addr(timer1_addr[3:2]), 
-	.write_enable(timer1_write_enable), 
-	.write_data(timer1_write_data), 
-	.read_result(timer1_read_result), 
-	.irq(timer1_irq)
+	.addr(switches_addr[2]), 
+	.write_enable(switches_write_enable), 
+	.write_data(switches_write_data), 
+	.read_result(switches_read_result), 
+	.irq(switches_irq), 
+
+	.dip_switch0(dip_switch0), 
+	.dip_switch1(dip_switch1), 
+	.dip_switch2(dip_switch2), 
+	.dip_switch3(dip_switch3), 
+	.dip_switch4(dip_switch4), 
+	.dip_switch5(dip_switch5), 
+	.dip_switch6(dip_switch6), 
+	.dip_switch7(dip_switch7)
+);
+
+led led(
+	.clk(clk), 
+	.rst(rst), 
+
+	.addr(led_addr), 
+	.write_enable(led_write_enable), 
+	.write_data(led_write_data), 
+	.read_result(led_read_result), 
+	.irq(led_irq), 
+
+	.led_light(led_light)
+);
+
+nixie nixie(
+	.clk(clk), 
+	.rst(rst), 
+
+	.addr(nixie_addr), 
+	.write_enable(nixie_write_enable), 
+	.write_data(nixie_write_data), 
+	.read_result(nixie_read_result), 
+	.irq(nixie_irq), 
+
+	.digital_tube0(digital_tube0), 
+	.digital_tube1(digital_tube1), 
+	.digital_tube2(digital_tube2), 
+	.digital_tube_sel0(digital_tube_sel0), 
+	.digital_tube_sel1(digital_tube_sel1), 
+	.digital_tube_sel2(digital_tube_sel2)
+);
+
+buttons buttons(
+	.clk(clk), 
+	.rst(rst), 
+
+	.addr(buttons_addr), 
+	.write_enable(buttons_write_enable), 
+	.write_data(buttons_write_data), 
+	.read_result(buttons_read_result), 
+	.irq(buttons_irq), 
+	
+	.user_key(user_key)
 );
 
 endmodule
