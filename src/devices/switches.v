8985d68c1747e4cf50f69a70161337ffaf67d6f3
a-l-r <alralr@buaa.edu.cn>
2019-12-21 13:07:51 +0000

    switches: add note for map between addresses and physical locations of the two packs of switches

--- src/devices/switches.v
+++ src/devices/switches.v
@@ -28,6 +28,7 @@ always @(posedge clk) begin
 	if (rst == 1'b1) begin
 		stored <= 0;
 	end else begin
+		/* NOTE: The upper pack of switches has a lower address. */
 		stored <= ~{dip_switch3, dip_switch2, dip_switch1, dip_switch0, 
 			dip_switch7, dip_switch6, dip_switch5, dip_switch4};
 	end
