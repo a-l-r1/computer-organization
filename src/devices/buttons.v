1fd0aaa4c37ab56fa0b3d549664f869805555f8a
a-l-r <alralr@buaa.edu.cn>
2019-12-23 01:09:36 +0800

    buttons: use combinatorial logic for irq

--- src/devices/buttons.v
+++ src/devices/buttons.v
@@ -12,32 +12,21 @@ module buttons(
 );
 
 reg [7:0] stored;
-reg irq_reg;
 
 initial begin
 	stored <= 0;
-	irq_reg <= 0;
 end
 
 always @(posedge clk) begin
 	if (rst == 1'b1) begin
 		stored <= 0;
-		irq_reg <= 0;
 	end else begin
 		stored <= ~user_key;
-		
-		/* NOTE: When a button is pushed, it emits 0 continuously. Assume that
-		 * only one button is being pushed down at a time. */
-		if ((&user_key) == 0) begin	
-			irq_reg <= 1'b1;
-		end else begin
-			irq_reg <= 1'b0;
-		end
 	end
 end
 
 assign read_result = {24'b0, stored};
-assign irq = irq_reg;
+assign irq = ~(&user_key);
 
 endmodule
 
