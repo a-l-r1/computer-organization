110a29058a07da1b32ab73e378edcc6e3aa22709
a-l-r <alralr@buaa.edu.cn>
2019-12-23 18:06:06 +0000

    buttons: add interrupt capability

--- src/devices/buttons.v
+++ src/devices/buttons.v
@@ -12,21 +12,32 @@ module buttons(
 );
 
 reg [7:0] stored;
+reg irq_reg;
 
 initial begin
 	stored <= 0;
+	irq_reg <= 0;
 end
 
 always @(posedge clk) begin
 	if (rst == 1'b1) begin
 		stored <= 0;
+		irq_reg <= 0;
 	end else begin
 		stored <= ~user_key;
+
+		/* NOTE: Have to declare truly separate conditions. */
+		if (&stored == 1'b0 && write_enable == 1'b0) begin
+			irq_reg <= 1'b1;
+		end
+		if (write_enable == 1'b1) begin
+			irq_reg <= 1'b0;
+		end
 	end
 end
 
 assign read_result = {24'b0, stored};
-assign irq = 1'b0;
+assign irq = irq_reg;
 
 endmodule
 
