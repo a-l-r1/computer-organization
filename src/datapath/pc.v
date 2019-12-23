dde0632f093db8b5a9f7b20e7c657ebce6e047fb
a-l-r <alralr@buaa.edu.cn>
2019-12-23 05:27:13 +0000

    pc: use own PC_START_ADDRESS

--- src/datapath/pc.v
+++ src/datapath/pc.v
@@ -13,12 +13,12 @@ module pc(
 reg [31:0] saved_pc;
 
 initial begin
-	saved_pc = `IM_START_ADDRESS;
+	saved_pc = `PC_START_ADDRESS;
 end
 
 always @(posedge clk) begin
 	if (rst == 1'b1) begin
-		saved_pc <= `IM_START_ADDRESS;
+		saved_pc <= `PC_START_ADDRESS;
 	end else begin
 		if (enable == `PC_ENABLED) begin
 			saved_pc <= next_pc;
--- src/include/pc.h
+++ src/include/pc.h
@@ -4,5 +4,7 @@
 `define PC_ENABLED 1'b1
 `define PC_DISABLED 1'b0
 
+`define PC_START_ADDRESS 32'hbfc03000
+
 `endif
 
